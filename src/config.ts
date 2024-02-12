ff2c7a52d0a74b82030bcdcf371a123216b4dcf1
vineet073 <vkiit11th@gmail.com>
2024-02-12 21:53:59 +0530

    test commit

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'http://ec2-65-0-76-204.ap-south-1.compute.amazonaws.com';
-// export const BASE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:3001';
