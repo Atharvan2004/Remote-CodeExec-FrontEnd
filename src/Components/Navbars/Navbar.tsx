5360d532f5a2cc123e88328023db5171f2b49f05
Rahull004 <rahulbansal24072004@gmail.com>
2024-03-09 10:59:03 +0530

    solving merge conflict

--- src/Components/Navbars/Navbar.tsx
+++ src/Components/Navbars/Navbar.tsx
@@ -7,25 +7,28 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../main";
 import { logout } from "../../services/authApi";
 
+
 const Navbar = () => {
   const [isRoomModalOpen, setIsRoomModalOpen] = useState(false);
   const { user } = useSelector((state: RootState) => state.profile);
-  const { token }=useSelector((state:RootState)=>state.auth);
+  const { token } = useSelector((state:RootState)=>state.auth);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const dispatch=useDispatch();
 
   return (
     <nav className=" flex h-[70px] w-full shrink-0 items-center px-5 border-2 border-green-700">
       <div className={`flex w-full items-center justify-between`}>
-        {user && (
+      
+        {
+          user && (
           <button
             onClick={() => setIsRoomModalOpen(true)}
-            className="bg-orange-600 py-2 px-3 cursor-pointer rounded text-slate-200 hover:bg-orange-500"
-          >
+            className="bg-orange-600 py-2 px-3 cursor-pointer rounded text-slate-200 hover:bg-orange-500">
             Code With Friends ?
           </button>
-        )}
-
+          )
+        }
+        
         <div>
           <Link to="/" className=" ">
             <img src="/logo1.png" alt="Logo" height={100} width={100} />
@@ -33,13 +36,14 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center space-x-3  justify-end">
-          {!token && !user && (
-            <>
-              <Link to="/login">
-                <button className="bg-green-700 py-2 px-3 cursor-pointer rounded text-slate-200">
-                  Log in
-                </button>
-              </Link>
+          {
+            !token && !user &&(
+              <>
+                 <Link to="/login">
+                  <button className="bg-green-700 py-2 px-3 cursor-pointer rounded text-slate-200">
+                    Log in
+                  </button>
+                </Link>
 
                 <Link to="/sign-up">
                   <button className="bg-green-700 py-2 px-3 cursor-pointer rounded text-slate-200">
@@ -49,9 +53,6 @@ const Navbar = () => {
               </>
             )
           }
-         
-
-          
           
           {
             user && token && (
@@ -59,15 +60,14 @@ const Navbar = () => {
                 <div className="">
                   <img src={user?.image} width={35} height={35} className="rounded-full"/>
                 </div>
-              </div>
-              <button
-                className="bg-slate-200 py-1.5 px-3 cursor-pointer rounded text-green-700 border-2 border-green-700"
-                onClick={() => dispatch<any>(logout(navigate))}
-              >
-                <FiLogOut />
-              </button>
-            </>
-          )}
+                <button className="bg-slate-200 py-1.5 px-3 cursor-pointer rounded text-green-700 border-2 border-green-700" 
+                onClick={() => dispatch<any>(logout(navigate))}>
+                  <FiLogOut />
+                </button>
+              </>
+            )
+          }
+          
         </div>
       </div>
       {isRoomModalOpen && (
