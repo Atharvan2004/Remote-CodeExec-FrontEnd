e2743b091fdc791ef24fd7f1761acd188d827ee7
Rahull004 <rahulbansal24072004@gmail.com>
2024-03-11 00:57:56 +0530

    adding loading screen spinner

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,17 @@ import RestrictedRoute from "./Components/Common/RestrictedRoute/RestrictedRoute
 import VerifyEmail from "./Pages/VerifyEmail.tsx";
 import ForgotPassword from "./Pages/ForgotPassword.tsx";
 import ResetPassword from "./Pages/ResetPassword.tsx";
-
+import { useState, useEffect } from "react";
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
-
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <>
       <div>
@@ -25,26 +32,44 @@ function App() {
                 secondary: "white",
               },
             },
-          }}>
-        </Toaster>
+          }}
+        ></Toaster>
       </div>
-
-
-      <BrowserRouter>
-        <Routes>
+      {loading ? (
+        <HashLoader
+          color="#36d64f"
+          size={100}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          cssOverride={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+          }}
+        />
+      ) : (
+        <BrowserRouter>
+          <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<SignInPage/>}/>
-            <Route path="/sign-up" element={<SignUpPage/>}/>
-            <Route path="/verify-email" element={<VerifyEmail/>}/>
-            <Route path="/forgot-password" element={<ForgotPassword/>}/>
-            <Route path="/update-password/:id" element={<ResetPassword/>}/>
-                        
-            <Route path="/room/:roomId" element={
-              <RestrictedRoute><Room/></RestrictedRoute>
-            } />
-        </Routes>
-      </BrowserRouter>
+            <Route path="/login" element={<SignInPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/verify-email" element={<VerifyEmail />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/update-password/:id" element={<ResetPassword />} />
 
+            <Route
+              path="/room/:roomId"
+              element={
+                <RestrictedRoute>
+                  <Room />
+                </RestrictedRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
   );
 }
